1d02fed39633b99810af547ab2c9b95907583a34
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2020-08-06 17:19:42 +0200

    Add Django 3.1 compatibility

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
